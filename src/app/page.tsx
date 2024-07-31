d8ccc2266bbf3a2f857cc1d4e8af53b93db5855b
zeyadsalah686 <zeyadsalah686@gmail.com>
2024-07-31 11:55:00 +0300

    add blue shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import MaxWidthWrapper from "@/components/MaxWidthWaper";
+
 export default function Home() {
     return (
         <>
-            dd
+            <MaxWidthWrapper>
+                <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+                    hello
+                </div>
+            </MaxWidthWrapper>
         </>
     );
 }
